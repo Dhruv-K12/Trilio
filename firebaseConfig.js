ad1ebb803f81fdda7671829f1872e4b068461090
Dhruv-Ku12 <dmontage000@gmail.com>
2025-07-04 01:21:35 +0530

    initail files with firebase config

---
+++ firebaseConfig.js
@@ -0,0 +1,12 @@
+import { initializeApp } from "firebase/app";
+
+const firebaseConfig = {
+  apiKey: "AIzaSyDD6D7l7JSz7oRnQA7nFFle6OAglsSnlJM",
+  authDomain: "trilio-ab33c.firebaseapp.com",
+  projectId: "trilio-ab33c",
+  storageBucket: "trilio-ab33c.firebasestorage.app",
+  messagingSenderId: "304565311303",
+  appId: "1:304565311303:web:4e89f31897043066c85e62",
+};
+
+const app = initializeApp(firebaseConfig);
